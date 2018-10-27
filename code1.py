a5ce81567ac1c67ae8d71d21ae16ea92ea7f0689
Harika Malapaka <hsmalapa@ncsu.edu>
2018-10-27 07:26:40 -0700

    lol

---
+++ code1.py
@@ -0,0 +1,27 @@
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
+import pandas as pd
+
+
+
+
+sc = SparkContext('local','example')  # if using locally
+df_business=sc.textFile('yelp_business.csv')
+df_business_Attributes=sc.textFile('yelp_business_attributes.csv')
+df_hours=sc.textFile('yelp_business_hours.csv')
+df_checkin=sc.textFile('yelp_checkin.csv')
+
+print("COllecting Business Data and printing")
+print(df_business.collect())
+
+print("------------------------------COllecting Business attributes data and printing")
+print(df_business_Attributes.collect())
+
+
+print("Collecting Business hours data and printing")
+print(df_hours.collect())
+
+print("COllecting Checkin Data and printing")
+print(df_checkin.collect())
+
+
