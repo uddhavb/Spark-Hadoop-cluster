e60e3a2708a6bf1568c15801f6d4c1013ee4a45e
Uddhav Bhosle <ubhosle@ncsu.edu>
2018-12-05 17:59:13 -0500

    setup - create roles

---
+++ setup/setup.sh
@@ -0,0 +1,9 @@
+echo "Program set up for Project"
+sudo apt-get update
+sudo apt-get install -y python-pip  
+pip install numpy 
+pip install kazoo
+pip install pandas
+sudo apt-get install scala
+tar xvf spark-2.3.2-bin-hadoop2.7.tgz
+
